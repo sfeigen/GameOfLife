defd40e109a597372637d3a529ec533ad618167e
Sam Feigenbaum <sfeigen@live.com>
2017-02-05 09:56:05 -0500

    changes

--- optimize_search.py
+++ optimize_search.py
@@ -1,4 +1,8 @@
 ''' optimizing search'''
+from time import time
+
+#benchmark
+T0 = time()
 
 TILE = {}
 STATE = ['Untouched', 'Populated', 'Underpopulated', 'Overpopulated']
@@ -12,27 +16,33 @@ def create_board(size):
 
 def alg(size):
     '''smarter search'''
+    create_board(size)
+
     corners = [1, size, (size * (size-1)) + 1, size*size]
     left = (size + 1) % size
+    interior = []
+
+    #get interior squares to check first
+    for i in range(size+1, size * (size-1)):
+        if i % size != (size + 1) % size and i % size != 0:
+            interior.append(i)
 
     for i in range(1, (size*size) + 1):
-        if i > (size) and i <= size * (size-1):
-            if i % size == left:
-                # left: up, down, right
-                print(i, "left")
-            elif i % size == 0:
-                # right: up, down, left
-                print(i, "right")
-            else:
-                #top, bottom, l, r
-                print(i, "interior")
-        elif i in corners:
-            # corners
-            print(i, "corner")
+        if i in interior:
+            print(i, "interior")
+        elif i % size == left and i < size * (size-1) + 1:
+            # left: up, down, rt
+            print(i, "left")
+        elif i % size == 0 and i <= size * (size-1):
+            # right: up, down, left
+            print(i, "right")
         elif i < size:
             # top
             print(i, "top")
-        elif i > (size * (size-1) + 1):
+        elif i in corners:
+            # corners
+            print(i, "corner")
+        else:
             # bottom
             print(i, "bottom")
 
@@ -45,3 +55,7 @@ def main(size):
     alg(size)
 
 main(5)
+
+#benchmark
+T1 = time()
+print(T1 - T0, "seconds.")
